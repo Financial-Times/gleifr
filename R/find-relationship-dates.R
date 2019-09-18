5a43c17f41697d8b0bfa082f32b57d056a0a3e19
Oli Elliott <oliver.elliott@ft.com>
2019-09-18 10:48:05 +0100

    read raw lei csvs

---
+++ R/find-relationship-dates.R
@@ -0,0 +1,70 @@
+
+
+clean_lei_relationship_dates <- function(raw_lei_relationships){
+
+	base_lei_rel <- raw_lei_relationships %>%
+		dplyr::mutate(row_id = dplyr::row_number())
+
+	p1 <- base_lei_rel %>%
+		dplyr::select(row_id,
+									start_date = Relationship.Period.1.startDate,
+									end_date = Relationship.Period.1.endDate,
+									period_type = Relationship.Period.1.periodType)
+
+	p2 <- base_lei_rel %>%
+		dplyr::select(row_id,
+									start_date = Relationship.Period.2.startDate,
+									end_date = Relationship.Period.2.endDate,
+									period_type = Relationship.Period.2.periodType)
+
+	p3 <- base_lei_rel %>%
+		dplyr::select(row_id,
+									start_date = Relationship.Period.3.startDate,
+									end_date = Relationship.Period.3.endDate,
+									period_type = Relationship.Period.3.periodType)
+
+	p4 <- base_lei_rel %>%
+		dplyr::select(row_id,
+									start_date = Relationship.Period.4.startDate,
+									end_date = Relationship.Period.4.endDate,
+									Relationship.Period.4.periodType)
+
+	p5 <- base_lei_rel %>%
+		dplyr::select(row_id,
+									start_date = Relationship.Period.5.startDate,
+									end_date = Relationship.Period.5.endDate,
+									period_type = Relationship.Period.5.periodType)
+
+	rel_dates <- dplyr::bind_rows(p1,p2,p3,p4,p5) %>%
+		dplyr::filter(period_type == "RELATIONSHIP_PERIOD") %>%
+		dplyr::select(row_id,
+									start_date,
+									end_date)
+
+	if(nrow(rel_dates) > dplyr::n_distinct(rel_dates$row_id)){
+		rel_dates <- rel_dates %>%
+			dplyr::group_by(row_id) %>%
+			dplyr::summarise(start_date = min(start_date),
+											 end_date = max(end_date))
+	}
+
+	clean_rel_dates <- rel_dates %>%
+		dplyr::mutate(
+			start_date = parse_lei_ts(start_date),
+			end_date = parse_lei_ts(end_date)
+		)
+
+	base_lei_rel %>%
+		dplyr::left_join(clean_rel_dates) %>%
+		dplyr::select(
+			start_node_id,
+			start_node_type,
+			end_node_id,
+			end_node_type,
+			relationship_type,
+			relationship_status,
+			relationship_start_date = start_date,
+			relationship_end_date = end_date
+		)
+
+}
---
+++ R/read-lei-csv.R
@@ -0,0 +1,79 @@
+
+
+read_gleif_entities_csv <- function(file){
+	#' Read GLEIF entities csv file
+	#'
+
+	readr::read_csv(file = file,
+									col_types = readr::cols_only(
+										LEI = "c",
+										Entity.LegalName = "c",
+										Entity.LegalAddress.Country = "c",
+										Entity.HeadquartersAddress.Country = "c",
+										Entity.RegistrationAuthority.RegistrationAuthorityID = "c",
+										Entity.EntityCategory = "c",
+										Entity.EntityStatus = "c",
+										Registration.InitialRegistrationDate = "c",
+										Registration.LastUpdateDate = "c",
+										Registration.RegistrationStatus = "c",
+										Registration.ValidationAuthority.ValidationAuthorityID = "c"
+									)) %>%
+		dplyr::rename(
+			lei = LEI,
+			entity_legal_name = Entity.LegalName,
+			entity_legal_country = Entity.LegalAddress.Country,
+			entity_hq_country = Entity.HeadquartersAddress.Country,
+			entity_registration_authority_id = Entity.RegistrationAuthority.RegistrationAuthorityID,
+			entity_category = Entity.EntityCategory,
+			entity_status = Entity.EntityStatus,
+			initial_registration_date = Registration.InitialRegistrationDate,
+			last_updated_date = Registration.LastUpdateDate,
+			registration_status = Registration.RegistrationStatus,
+			validation_authority_id = Registration.ValidationAuthority.ValidationAuthorityID
+		) %>%
+		dplyr::mutate(
+			initial_registration_date = parse_lei_ts(initial_registration_date),
+			last_updated_date = parse_lei_ts(last_updated_date)
+		)
+}
+
+
+read_gleif_relationships_csv <- function(file){
+	#' Read GLEIF relationships csv file
+	#'
+
+	readr::read_csv(file = file,
+									col_types = readr::cols_only(
+										Relationship.StartNode.NodeID = "c",
+										Relationship.StartNode.NodeIDType = "c",
+										Relationship.EndNode.NodeID = "c",
+										Relationship.EndNode.NodeIDType = "c",
+										Relationship.RelationshipType = "c",
+										Relationship.RelationshipStatus = "c",
+										Relationship.Period.1.startDate = "c",
+										Relationship.Period.1.endDate = "c",
+										Relationship.Period.1.periodType = "c",
+										Relationship.Period.2.startDate = "c",
+										Relationship.Period.2.endDate = "c",
+										Relationship.Period.2.periodType = "c",
+										Relationship.Period.3.startDate = "c",
+										Relationship.Period.3.endDate = "c",
+										Relationship.Period.3.periodType = "c",
+										Relationship.Period.4.startDate = "c",
+										Relationship.Period.4.endDate = "c",
+										Relationship.Period.4.periodType = "c",
+										Relationship.Period.5.startDate = "c",
+										Relationship.Period.5.endDate = "c",
+										Relationship.Period.5.periodType = "c"
+									)) %>%
+		dplyr::rename(
+			start_node_id = Relationship.StartNode.NodeID,
+			start_node_type = Relationship.StartNode.NodeIDType,
+			end_node_id = Relationship.EndNode.NodeID,
+			end_node_type = Relationship.EndNode.NodeIDType,
+			relationship_type = Relationship.RelationshipType,
+			relationship_status = Relationship.RelationshipStatus
+		) %>%
+		clean_lei_relationship_dates()
+
+}
